c90c6346641740d7f523a3415483bd7035ac1794
RenatoCalisto <108838751+RenatoCalisto@users.noreply.github.com>
2023-09-17 18:57:03 -0300

    TCC 1.2

---
+++ TCC 1.2/cursos.php
@@ -0,0 +1,313 @@
+
+
+
+
+
+<?php
+
+require_once("header.php");
+
+?>
+
+<body>
+
+
+
+
+
+    <nav class="navbar fixed-top navbar-expand-lg bg-primary " data-bs-theme="dark">
+        <div class="container-fluid">
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            
+            <a class="nav-link active text-white " aria-current="page"  href="index.php">Home</a>
+            
+            <ul class="navbar-nav ms-auto  mb-2 mb-lg-0">
+              <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Cursos
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="cursoadm.php">Administração</a></li>
+                  <li><a class="dropdown-item" href="cursocom.php">Comércio</a></li>
+                  <li><a class="dropdown-item" href="cursodes.php">Desenvolvimento de Sistemas</a></li>
+                  <li><a class="dropdown-item" href="cursoguia.php">Guia de Turismo</a></li>
+                  <li><a class="dropdown-item" href="cursosec.php">Secretariado</a></li>
+                  <li><a class="dropdown-item" href="cursotran.php">Transações Imobiliárias</a></li>
+                </ul>
+              </li>
+
+            </ul>
+
+
+            <form class="d-flex ms-auto" role="search">
+              <a href="home.php"><button class="btn btn-outline-success text-white" type="button">Login</button></a>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="text-center"><section id="topo">
+    <br>
+    <br>
+    <br>
+    <br><img src="img/Atomologo.png" alt="Logo da Escola"></section>
+</div>
+
+    <h1 class="text-center">
+Atomo - Escola Técnica EaD
+<br>
+<br>
+    </h1>
+
+
+
+      <div class="container">
+        <div class="text-center">
+                                <h1 class="title">Cursos</h1>
+        </div>
+    </div>
+
+
+
+      <section id="features" class="section feature">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <div class="single-feature text-center text-lg-left ">
+                                <a href="https://www.youtube.com/watch?v=f4G_JxAM_eE&t=11s&ab_channel=Estrat%C3%A9giaConcursos"><img src="img/indexcursoadm.jpg" alt="foto de um administrador assinando um papel" class="img-fluid feature-img" ></a>
+                                <div class="space-md"></div>
+                                <h3 class="title">Curso de Administração</h3>
+                                <div class="space-sm"></div>
+                                <p class="lead">
+                                    Organizado em três módulos, o curso oferece a oportunidade de certificação a cada etapa estudada e conta com material didático composto por roteiros de estudo, livros em formato pdf, videoaulas e objetos interativos de estudo.
+                                        <br>
+                                    O <strong>Técnico em Administração</strong> é o profissional que executa operações administrativas relativas a protocolos e arquivos, confecção e expedição de documentos e controle de estoques. Aplica conceitos e modelos de gestão em funções administrativas. Opera sistemas de informações gerenciais de pessoal e de materiais.
+                                </p>
+
+                                <form class=" text-center" role="search">
+              <a href="https://www.youtube.com/watch?v=f4G_JxAM_eE&t=11s&ab_channel=Estrat%C3%A9giaConcursos"><button class="btn btn-outline-success" type="button">Cursar Administração</button></a>
+            </form>
+
+
+
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="single-feature text-center text-lg-left">
+                                <a href="https://www.youtube.com/watch?v=lQV5myhSqCs&list=PLUkv8hZ8kunVjEKfgPdLQlD_Nk1vdILgq&ab_channel=ShirleysonKaisser"><img src="img/indexcursocomercio.jpg" alt="foto de um comerciante vendendo" class="img-fluid feature-img"></a>
+                                <div class="space-md"></div>
+                                <h3 class="title">Curso de Comércio</h3>
+                                <div class="space-sm"></div>
+                                <p class="lead">             
+                                    Organizado em três módulos, o curso oferece a oportunidade de certificação a cada etapa estudada e conta com material didático composto por roteiros de estudo, livros em formato pdf, videoaulas e objetos interativos de estudo.
+                                        <br>
+                                    O <strong>Técnico em Comércio</strong> é o profissional que aplica métodos de comercialização de bens e serviços. Efetua controle quantitativo e qualitativo de produtos. Coordena a armazenagem no estabelecimento comercial. Elabora planilha de custos. Verifica a demanda e comunica previsões aos fornecedores. Operacionaliza planos de marketing e comunicação, logística, recursos humanos e comercialização.
+                                </p>
+
+                                <form class=" text-center" role="search">
+              <a href="https://www.youtube.com/watch?v=lQV5myhSqCs&list=PLUkv8hZ8kunVjEKfgPdLQlD_Nk1vdILgq&ab_channel=ShirleysonKaisser"><button class="btn btn-outline-success" type="button">Cursar Comércio</button></a>
+            </form>
+
+
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="single-feature text-center text-lg-left">
+                                <a href="https://www.youtube.com/watch?v=ANtXEpC3488&list=PLxNM4ef1BpxjLIq-eTL8mgROdviCiobs9&ab_channel=PortalHugoCursos"><img src="img/indexcursodesenvolvedor.jpg" alt="foto de um programador de computadores" class="img-fluid feature-img"></a>
+                                <div class="space-md"></div>
+                                <h3 class="title">Curso de Desenvolvimento de Sistemas</h3>
+                                <div class="space-sm"></div>
+                                <p class="lead">
+                                    Organizado em três módulos, o curso oferece a oportunidade de certificação a cada etapa estudada e conta com material didático composto por roteiros de estudo, livros em formato pdf, videoaulas, objetos interativos de estudo e aulas práticas Online.
+                                        <br>
+                                    O <strong>Técnico em Desenvolvimento de Sistemas</strong> é o profissional que analisa e projeta sistemas. Constrói, documenta, realiza testes e mantém sistemas de informação. Utiliza ambientes de desenvolvimento e linguagens de programação específica. Modela, implementa e mantém bancos de dados.
+                                </p>
+
+                                <form class=" text-center" role="search">
+              <a href="https://www.youtube.com/watch?v=ANtXEpC3488&list=PLxNM4ef1BpxjLIq-eTL8mgROdviCiobs9&ab_channel=PortalHugoCursos"><button class="btn btn-outline-success" type="button">Cursar Desenvolvimento de Sistemas</button></a>
+            </form>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <br>
+
+    <section id="features" class="section feature">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <div class="single-feature text-center text-lg-left ">
+                                <a href="https://www.youtube.com/watch?v=Afmz9zmn_E0&ab_channel=GeovannaSilva-VivadeTurismo"><img src="img/indexcursoguia.jpg" alt="foto de uma guia de turismo em uma canoa no meio de um rio" class="img-fluid feature-img" ></a>
+                                <div class="space-md"></div>
+                                <h3 class="title">Curso de Guia de Turismo</h3>
+                                <div class="space-sm"></div>
+                                <p class="lead">
+                                    Organizado em dois módulos, o curso oferece a oportunidade de certificação a cada etapa estudada e conta com material didático composto por roteiros de estudo, livros em formato pdf,  videoaulas e objetos interativos de estudo.
+                                        <br>
+                                    O <strong>Técnico em Guia de Turismo</strong> é o profissional que conduz e assiste pessoas ou grupos em traslados, passeios, visitas e viagens. Informa os visitantes sobre aspectos socioculturais, históricos, ambientais e geográficos. Traduz o patrimônio material e imaterial de uma região para visitantes. Estrutura e apresenta roteiros e itinerários turísticos de acordo com interesses, expectativas ou necessidades específicas.                                  
+                                </p>
+
+
+                                <form class=" text-center" role="search">
+              <a href="https://www.youtube.com/watch?v=Afmz9zmn_E0&ab_channel=GeovannaSilva-VivadeTurismo"><button class="btn btn-outline-success" type="button">Cursar Turismo</button></a>
+            </form>
+
+
+
+
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="single-feature text-center text-lg-left">
+                                <a href="https://www.youtube.com/watch?v=a1S6ayftKrI&list=PLkV5XCj-ThD3oJ_FSEEZVgBAQ7l9rvjJ9&ab_channel=AlexB.Lima"><img src="img/indexcursosecretariado.jpg" alt="foto de uma secretária trabalhando em frente a um computador" class="img-fluid feature-img" ></a>
+                                
+                                <div class="space-md"></div>
+                                <h3 class="title">Curso de Secretariado </h3>
+                                <div class="space-sm"></div>
+                                <p class="lead">
+                                    Organizado em três módulos, o curso oferece a oportunidade de certificação a cada etapa estudada e conta com material didático composto por roteiros de estudo, livros em formato pdf, videoaulas e objetos interativos de estudo.
+                                        <br>
+                                    O <strong>Técnico em Secretariado</strong> é o profissional que organiza a rotina diária e mensal da chefia ou direção para o cumprimento dos compromissos agendados. Estabelece os canais de comunicação da chefia ou direção com interlocutores, internos e externos, em língua nacional e estrangeira. Organiza tarefas relacionadas com o expediente geral do secretariado da chefia ou direção. Controla e arquiva documentos. Preenche e confere documentação de apoio à gestão organizacional. Utiliza aplicativos e a internet na elaboração, organização e pesquisa de informação.
+                                </p>
+
+                                <form class=" text-center" role="search">
+              <a href="https://www.youtube.com/watch?v=a1S6ayftKrI&list=PLkV5XCj-ThD3oJ_FSEEZVgBAQ7l9rvjJ9&ab_channel=AlexB.Lima"><button class="btn btn-outline-success" type="button">Cursar Secretariado</button></a>
+            </form>
+
+
+
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="single-feature text-center text-lg-left">
+                                <a href="https://www.youtube.com/watch?v=zeczTbpm8rg&ab_channel=TVCRECI"><img src="img/indextransimobiliaria.jpg" alt="foto de chaves em frente a uma casa" class="img-fluid feature-img"></a>
+                                <div class="space-md"></div>
+                                <h3 class="title">Curso de Transações Imobiliárias</h3>
+                                <div class="space-sm"></div>
+                                <p class="lead">
+                                    Organizado em dois módulos, o curso oferece a oportunidade de certificação ao final do segundo módulo e conta com o material didático composto por roteiros de estudo, libros em formato pdf, videoaulas e objetos interativos de estudo.
+                                        <br>
+                                    O <strong>Técnico em Transações Imobiliárias</strong> é o profissional que está habilitado para intermediar a compra, a venda, a locação e a permuta de imóveis. Atua na administração de bens imóveis, solicita e orienta quanto à documentação necessária para cadastramento, transferência, inscrição e registro junto aos órgãos competentes. Apresenta os imóveis aos clientes potenciais e orienta investimentos no mercado imobiliário. Realiza pesquisa de mercado, bem como a captação de imóveis, elaborando estratégias de demonstração e comercialização. Atua como avaliador de imóveis urbanos e rurais. Emite parecer técnico de avaliação mercadológica e comercial sobre o bem a ser comercializado. Interpreta projetos arquitetônicos e mapas. Acompanha a movimentação do mercado para antever tendências que afetam o empreendimento. Presta assessoria aos clientes após a transação imobiliária.
+                                    <div class="space-sm"></div>
+                                </p>
+
+
+                                <form class=" text-center" role="search">
+              <a href="https://www.youtube.com/watch?v=zeczTbpm8rg&ab_channel=TVCRECI"><button class="btn btn-outline-success" type="button">Cursar Transações Imobiliárias</button></a>
+            </form>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+            </div>        
+        <div class="space-md"></div>
+        </div>
+    </section>
+    </div>
+    </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="bg-primary text-white">
+        <div class="container">
+          <div class="row py-4">
+            <div class="col-12 col-md text-center mx-auto">
+              
+              <h3>Atomo</h3>
+              <h4>Cursos Técnicos</h4> 
+              <p class="text-white-50 text-small">
+                Mais de 150 tipos de cursos técnicos gratuitos<br> 
+                Presente em 156 municípios brasileiros, como o total de 212 polos Atomos no país. <br>
+                208 mil estudantes <br>    
+                Desde 1911<br>
+ 
+              </p>
+            </div>
+            <div class="col-12 col-md text-center mx-auto">
+              <h3> Links</h3>
+              <ul>
+                <a href="sobre.php" class="text-white">Quem somos nós</a><br>
+              </ul>
+            </div>
+            <div class="col-12 col-md text-center mx-auto">
+              <h3> <section id="sociais"> </h3>
+              <ul>
+<img src="img/Atomologo.png" width="150" alt="Logo da Escola">
+                <br>
+                <div class="copyright text-center text-lg-left">
+                  &copy; Copyright. All rights reserved.
+              </ul></section>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+    crossorigin="anonymous"
+></script>
+
+</body>
+</html>
+
---
+++ TCC 1.2/footer.php
@@ -0,0 +1,35 @@
+
+<footer class="bg-primary text-white">
+        <div class="container">
+          <div class="row py-4">
+            <div class="col-12 col-md text-center mx-auto">
+              
+              <h3>Atomo</h3>
+              <h4>Cursos Técnicos</h4> 
+              <p class="text-white-50 text-small">
+                Mais de 150 tipos de cursos técnicos gratuitos<br> 
+                Presente em 156 municípios brasileiros, como o total de 212 polos Atomos no país. <br>
+                208 mil estudantes <br>    
+                Desde 1911<br>
+ 
+              </p>
+            </div>
+            <div class="col-12 col-md text-center mx-auto">
+              <h3> Links</h3>
+              <ul>
+                <a href="sobre.php" class="text-white">Quem somos nós</a><br>
+              </ul>
+            </div>
+            <div class="col-12 col-md text-center mx-auto">
+              <h3> <section id="sociais"> </h3>
+              <ul>
+<img src="img/Atomologo.png" width="150" alt="Logo da Escola">
+                <br>
+                <div class="copyright text-center text-lg-left">
+                  &copy; Copyright. All rights reserved.
+              </ul></section>
+            </div>
+          </div>
+        </div>
+      </footer>
+
